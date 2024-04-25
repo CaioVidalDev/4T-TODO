b29ac64bda3f682836ad0b2db3429fdc1ce8cb54
Caio Vidal Silva <caio.vidal@4tsolucoes.com.br>
2024-04-25 18:14:14 -0300

    HTML e CSS atualizado no Spring Boot

--- src/main/java/com/example/t4td0/HomeController.java
+++ src/main/java/com/example/t4td0/HomeController.java
@@ -1,28 +1,16 @@
 package com.example.t4td0;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-;
 
 @Controller
 public class HomeController {
 
-    
     @GetMapping("/")
-    public ModelAndView index() {
-
-        ModelAndView mv = new ModelAndView();
-        mv.setViewName("home/projeto-tarefas/index.html");
-
-        return mv;
-
+    public String index() {
+        return "home/projeto-tarefas/index";
     }
-
-} 
-
+}
 
 
-
-    
 
