f7530a8bb93ea5e7f32ee737708bb777d9b34c9d
Caio Vidal Silva <caio.vidal@4tsolucoes.com.br>
2024-05-06 16:31:07 -0300

    Adicionando mensagem de ação em todas as telas

--- src/main/java/com/example/t4td0/controller/HomeController.java
+++ src/main/java/com/example/t4td0/controller/HomeController.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/tarefas")
@@ -17,7 +19,7 @@ public class HomeController {
     private TarefaRepository tarefaRepository;
 
     @GetMapping
-    public String index(Model model) {
+    public String listarTarefas(Model model) {
         List<Tarefa> tarefas = tarefaRepository.findAll();
         model.addAttribute("tarefas", tarefas);
         return "home/projeto-tarefas/index";
@@ -30,18 +32,19 @@ public class HomeController {
     }
 
     @PostMapping("/new")
-    public String cadastrarTarefa(@ModelAttribute Tarefa tarefa) {
+    public String cadastrarTarefa(@ModelAttribute Tarefa tarefa, RedirectAttributes redirectAttributes) {
         if (tarefa != null && tarefa.getTitulo() != null && !tarefa.getTitulo().isEmpty()) {
             tarefaRepository.save(tarefa);
+            redirectAttributes.addFlashAttribute("cadastroSucesso", true);
         }
-        return "redirect:/tarefas";
+        return "redirect:/tarefas/new";
     }
 
     @GetMapping("/{id}/edit")
     public String preencherFormularioEdicao(@PathVariable("id") Long id, Model model) {
-        Tarefa tarefa = tarefaRepository.findById(id).orElse(null);
-        if (tarefa != null) {
-            model.addAttribute("tarefa", tarefa);
+        Optional<Tarefa> optionalTarefa = tarefaRepository.findById(id);
+        if (optionalTarefa.isPresent()) {
+            model.addAttribute("tarefa", optionalTarefa.get());
             return "home/projeto-tarefas/editar-tarefa";
         } else {
             return "redirect:/tarefas";
@@ -49,40 +52,40 @@ public class HomeController {
     }
 
     @PostMapping("/edit")
-    public String editarTarefa(@ModelAttribute Tarefa tarefa) {
+    public String editarTarefa(@ModelAttribute Tarefa tarefa, RedirectAttributes redirectAttributes) {
         if (tarefa != null) {
             tarefaRepository.save(tarefa);
+            redirectAttributes.addFlashAttribute("edicaoSucesso", true);
         }
-        return "redirect:/tarefas";
+        return "redirect:/tarefas/" + tarefa.getId() + "/edit";
     }
 
     @GetMapping("/{id}/destroy")
     public String preencherFormularioExclusao(@PathVariable("id") Long id, Model model) {
-        Tarefa tarefa = tarefaRepository.findById(id).orElse(null);
-        if (tarefa != null) {
-            model.addAttribute("tarefa", tarefa);
+        Optional<Tarefa> optionalTarefa = tarefaRepository.findById(id);
+        if (optionalTarefa.isPresent()) {
+            model.addAttribute("tarefa", optionalTarefa.get());
             return "home/projeto-tarefas/excluir-tarefa";
         } else {
             return "redirect:/tarefas";
         }
     }
 
-    @PostMapping("/destroy")
-    public String excluirTarefa(@RequestParam("id") Long id) {
+    @PostMapping("/{id}/destroy")
+    public String excluirTarefa(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
         tarefaRepository.deleteById(id);
-        return "redirect:/tarefas";
+        redirectAttributes.addFlashAttribute("exclusaoSucesso", true);
+        return "redirect:/tarefas/" + id + "/destroy";
     }
 
     @GetMapping("/{id}")
     public String preencherFormularioVisualizacao(@PathVariable("id") Long id, Model model) {
-        Tarefa tarefa = tarefaRepository.findById(id).orElse(null);
-        if (tarefa != null) {
-            model.addAttribute("tarefa", tarefa);
+        Optional<Tarefa> optionalTarefa = tarefaRepository.findById(id);
+        if (optionalTarefa.isPresent()) {
+            model.addAttribute("tarefa", optionalTarefa.get());
             return "home/projeto-tarefas/visualizar-tarefa";
         } else {
             return "redirect:/tarefas";
         }
     }
 }
-
-
