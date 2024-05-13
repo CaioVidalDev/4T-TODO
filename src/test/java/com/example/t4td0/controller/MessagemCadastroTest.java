8f4c699b6a27705461fc72b7d7c3b4a21ed2c5ce
Caio Vidal Silva <caio.vidal@4tsolucoes.com.br>
2024-05-13 17:57:12 -0300

    Concluido Teste Automatizado de Mensagem de  Cadastrado

--- src/test/java/com/example/t4td0/controller/MessagemCadastroTest.java
+++ src/test/java/com/example/t4td0/controller/MessagemCadastroTest.java
@@ -16,4 +16,42 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class MessagemCadastroTest {
 
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void setUp() {
+        WebDriverManager.firefoxdriver().setup();
+        FirefoxOptions options = new FirefoxOptions();
+        driver = new FirefoxDriver(options);
+    }
+
+    @Test
+    public void testCadastroTarefa() {
+        driver.get("http://localhost:8080/tarefas");
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+       WebElement cadastrar = driver.findElement(By.className("btn-voltar"));
+       cadastrar.click();
+
+       WebElement caixaDeBusca = driver.findElement(By.id("titulo-cadastro"));
+       caixaDeBusca.click();
+       caixaDeBusca.sendKeys("Teste De Cadastrar");
+
+       WebElement salvarCadastro = driver.findElement(By.id("salvarCadastro"));
+       salvarCadastro.click();
+
+       /* Mensagem de Cadastrado com Sucesso */
+       WebElement cadastroSucess = driver.findElement(By.id("mensagemCadastroSucess"));
+       String mensagemCadastroSucess = cadastroSucess.getText();
+       
+       if(mensagemCadastroSucess.contains("Tarefa cadastrada com sucesso!")) {
+       System.out.println("Teste de Mensagem de cadastro de tarefa realizado com sucesso");
+       }else{
+        System.out.println("Erro na Pesquisa");
+       }
+
+       driver.close();
+
+    }
 }
