5f7c0741cadba1d4bb7bcfaedb5500459fb03f92
Caio Vidal Silva <caio.vidal@4tsolucoes.com.br>
2024-05-20 17:44:38 -0300

    Atualizando PUT pot PATCH and Adicionando comentario no Controller

--- src/main/java/com/example/t4td0/controller/HomeController.java
+++ src/main/java/com/example/t4td0/controller/HomeController.java
@@ -16,91 +16,91 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller // Anotação que define que esta classe é um controlador
-@RequestMapping("/tarefas") // Mapeia URLs que começam com "/tarefas" para este controlador.
+@RequestMapping("/tarefas") // Mapeia URL que começam com "/tarefas" para este controlador
 public class HomeController {
 
     @Autowired
-    private TarefaRepository tarefaRepository;
+    private TarefaRepository tarefaRepository; // Injeta uma instancia de TarefaRepository
 
     @Autowired
-    private TarefaValidator tarefaValidator;
-    // Anotação que indica ao Spring para injetar instâncias de TarefaRepository e TarefaValidator automaticamente.
+    private TarefaValidator tarefaValidator; // Injeta uma instancia de TarefaValidator
+   
     
-    @InitBinder // @InitBinder = Método que é chamado para inicializar o binder que realiza a vinculação de dados da web.
-    protected void initBinder(WebDataBinder binder) {  // WebDataBinder = Interface que permite a personalização da vinculação de dados da web.
-        binder.setValidator(tarefaValidator);
+    @InitBinder // Método para inicializar o WebDataBinder
+    protected void initBinder(WebDataBinder binder) { // WebDataBinder permite a conexão de dados da web
+        binder.setValidator(tarefaValidator); // Define o validador para Tarefa
     }
 
-    @GetMapping
-    public String listarTarefas(Model model) {  //model É um Objeto do tipo Model usado para passar dados para a visualização.
-        List<Tarefa> tarefas = tarefaRepository.findAll(); // tarefas É uma lista de objetos do tipo Tarefa que contém todas as tarefas recuperadas do banco de dados.
-        model.addAttribute("tarefas", tarefas);
-        return "home/projeto-tarefas/index";
+    @GetMapping // Realiza requisições de busca
+    public String listarTarefas(Model model) {  // O objeto Model é usado para passar dados para a visualização (Web/Pagina lista)
+        List<Tarefa> tarefas = tarefaRepository.findAll(); // Encontra todas as tarefas do banco de dados
+        model.addAttribute("tarefas", tarefas); // Adiciona/Atribui todas as tarefas na lista
+        return "home/projeto-tarefas/index"; // Retorna ao HTML de tarefas
     }
 
-    @GetMapping("/new")
-    public String cadastrarTarefaPage(Model model) { //model É um Objeto do tipo Model usado para passar dados para a visualização.
-        model.addAttribute("tarefa", new Tarefa());
-        return "home/projeto-tarefas/cadastrar-tarefa";
+    @GetMapping("/new") // Mapeia requisições de busca
+    public String cadastrarTarefaPage(Model model) { // Model É um objeto para passar dados para a visualização (da pagina de cadastro)
+        model.addAttribute("tarefa", new Tarefa()); // Adiciona uma nova tarefa ao Model (Web/Pagina New)
+        return "home/projeto-tarefas/cadastrar-tarefa"; // Retorna ao HTML de cadastrar
     }
 
-    @PostMapping("/new")
+    @PostMapping("/new") // Realiza requisições de adicionar
     public String cadastrarTarefa(@Validated @ModelAttribute Tarefa tarefa, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-        if (bindingResult.hasErrors()) {
-            return "home/projeto-tarefas/cadastrar-tarefa";
+        if (bindingResult.hasErrors()) { // Verifica se tem erro de validação
+            return "home/projeto-tarefas/cadastrar-tarefa"; // Se tiver erros ele retorna ao HTML de cadastrar
         }
-        tarefaRepository.save(tarefa);
-        redirectAttributes.addFlashAttribute("cadastroSucesso", true);
-        return "redirect:/tarefas/new";
+        tarefaRepository.save(tarefa); // Salva  nova tarefa no banco de dados
+        redirectAttributes.addFlashAttribute("cadastroSucesso", true); // Adiciona FLASH para mostrar que a tarefa foi cadastrada com sucesso
+        return "redirect:/tarefas/new"; // Redireciona a pagina a pagina de cadastro
     }
 
-    @GetMapping("/{id}/edit")
-    public String preencherFormularioEdicao(@PathVariable("id") Long id, Model model) {
-        Optional<Tarefa> optionalTarefa = tarefaRepository.findById(id);
-        if (optionalTarefa.isPresent()) {
-            model.addAttribute("tarefa", optionalTarefa.get());
-            return "home/projeto-tarefas/editar-tarefa";
+    @GetMapping("/{id}/edit") // Realiza requisições de busca
+    public String preencherFormularioEdicao(@PathVariable("id") Long id, Model model) { // O objeto Model é usado para passar dados (+id) para a visualização (Web/Pagina Edit)
+        Optional<Tarefa> optionalTarefa = tarefaRepository.findById(id); // Chama a tarefa pelo id
+        if (optionalTarefa.isPresent()) { // Se a tarefa opicional/selecionada estiver presente 
+            model.addAttribute("tarefa", optionalTarefa.get()); // Recupera/seleciona a tarefa ao Model (Web/Pagina Edit)
+            return "home/projeto-tarefas/editar-tarefa"; // Retorna ao HTML de editar
         } else {
-            return "redirect:/tarefas";
+            return "redirect:/tarefas"; // Se não, redireciona a pagina de tarefas
         }
     }
 
-    @PutMapping("/edit")
+    @PatchMapping("/edit") // Realiza requisições de atualizar
     public String editarTarefa(@Validated @ModelAttribute Tarefa tarefa, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
-        if (bindingResult.hasErrors()) {
-            return "home/projeto-tarefas/editar-tarefa";
+        if (bindingResult.hasErrors()) { // Verifica se tem erro de validação
+            return "home/projeto-tarefas/editar-tarefa"; // Retorna ao HTML de editar
         }
-        tarefaRepository.save(tarefa);
-        redirectAttributes.addFlashAttribute("edicaoSucesso", true);
-        return "redirect:/tarefas/" + tarefa.getId() + "/edit";
+        tarefaRepository.save(tarefa); // Salva tarefa no banco de dados
+        redirectAttributes.addFlashAttribute("edicaoSucesso", true); // Adiciona FLASH para mostrar que a tarefa foi editada com sucesso
+        return "redirect:/tarefas/" + tarefa.getId() + "/edit"; // Redireciona a pagina edição
     }
 
-    @GetMapping("/{id}/destroy")
-    public String preencherFormularioExclusao(@PathVariable("id") Long id, Model model) {
-        Optional<Tarefa> optionalTarefa = tarefaRepository.findById(id);
-        if (optionalTarefa.isPresent()) {
-            model.addAttribute("tarefa", optionalTarefa.get());
-            return "home/projeto-tarefas/excluir-tarefa";
+    @GetMapping("/{id}/destroy") // Realiza requisições de busca
+    public String preencherFormularioExclusao(@PathVariable("id") Long id, Model model) { // O objeto Model é usado para passar dados (+ id) para a visualização (Web/Pagina Destroy)
+        Optional<Tarefa> optionalTarefa = tarefaRepository.findById(id); // Chama a tarefa pelo id
+        if (optionalTarefa.isPresent()) { // Se a tarefa opicional/selecionada estiver presente
+            model.addAttribute("tarefa", optionalTarefa.get()); // Recupera/seleciona a tarefa ao Model (Web/Pagina Destroy)
+            return "home/projeto-tarefas/excluir-tarefa"; // Retorna ao HTML de excluir
         } else {
-            return "redirect:/tarefas";
+            return "redirect:/tarefas"; // Redireciona a pagina tarefas
         }
     }
 
-    @DeleteMapping("/{id}/destroy")
-    public String excluirTarefa(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
-        tarefaRepository.deleteById(id);
-        redirectAttributes.addFlashAttribute("exclusaoSucesso", true);
-        return "redirect:/tarefas";
+    @DeleteMapping("/{id}/destroy") // Realiza requisições de excluir
+    public String excluirTarefa(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) { 
+        tarefaRepository.deleteById(id); // Exclui a tarefa pelo id
+        redirectAttributes.addFlashAttribute("exclusaoSucesso", true); // Adiciona FLASH para mostrar que a tarefa foi excluida com sucesso
+        return "redirect:/tarefas"; // Redireciona a pagina tarefas
     }
 
-    @GetMapping("/{id}")
-    public String preencherFormularioVisualizacao(@PathVariable("id") Long id, Model model) {
-        Optional<Tarefa> optionalTarefa = tarefaRepository.findById(id);
-        if (optionalTarefa.isPresent()) {
-            model.addAttribute("tarefa", optionalTarefa.get());
-            return "home/projeto-tarefas/visualizar-tarefa";
+    @GetMapping("/{id}") // Realiza requisições de busca/visualização
+    public String preencherFormularioVisualizacao(@PathVariable("id") Long id, Model model) { // O objeto Model é usado para passar dados (+ id) para a visualização (Web/Pagina View)
+        Optional<Tarefa> optionalTarefa = tarefaRepository.findById(id); // Chama a tarefa pelo id
+        if (optionalTarefa.isPresent()) { // Se a tarefa opicional/selecionada estiver presente
+            model.addAttribute("tarefa", optionalTarefa.get()); // Recupera/seleciona a tarefa ao Model (Web/Pagina View)
+            return "home/projeto-tarefas/visualizar-tarefa"; // Retorna ao HTML de visualizar
         } else {
-            return "redirect:/tarefas";
+            return "redirect:/tarefas"; // Se não redireciona a pagina tarefas
         }
     }
 }
